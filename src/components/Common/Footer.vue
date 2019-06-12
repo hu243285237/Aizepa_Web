23cc5faf59c15c9d7ddc451b82f6edf145913cae
hu243285237 <243285237@qq.com>
2019-06-12 23:14:34 +0800

    更新页眉页脚

--- src/components/Common/Footer.vue
+++ src/components/Common/Footer.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="footer">
-    <div class="footer-text">
-      <a class="footer-btn" href="#">首页</a>
-      <a class="footer-btn" href="#">产品中心</a>
-      <a class="footer-btn" href="#">解决方案</a>
-      <a class="footer-btn" href="#">服务支持</a>
-      <a class="footer-btn" href="#">公司介绍</a>
-    </div>
-    <div class="footer-text">地址：广东省深圳市龙岗区坂雪岗大道3012号坂田商务大厦726室</div>
-    <div class="footer-text">电话：+86-755-85207786</div>
-    <div class="footer-text">传真：+86-755-85207781</div>
-    <div class="footer-text">邮件：jack.wang@aizepa.com</div>
+    <el-container>
+      <el-footer height="240px">
+        <div class="footer-item">
+          <a class="footer-btn" href="#">首页</a>
+          <a class="footer-btn" href="#">产品中心</a>
+          <a class="footer-btn" href="#">解决方案</a>
+          <a class="footer-btn" href="#">服务支持</a>
+          <a class="footer-btn" href="#">公司介绍</a>
+        </div>
+        <div class="footer-item">地址：广东省深圳市龙岗区坂雪岗大道3012号坂田商务大厦726室</div>
+        <div class="footer-item">电话：+86-755-85207786</div>
+        <div class="footer-item">传真：+86-755-85207781</div>
+        <div class="footer-item">邮件：jack.wang@aizepa.com</div>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -26,14 +30,14 @@ export default {
 </script>
 
 <style scoped>
-.footer {
+.el-footer {
   text-align: left;
   background-color: rgb(31, 32, 36);
   color: aliceblue;
   padding: 30px;
 }
 
-.footer-text {
+.footer-item {
   margin-top: 10px;
   margin-bottom: 10px;
 }
--- src/components/Common/Header.vue
+++ src/components/Common/Header.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="header">
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu"
-      mode="horizontal"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
-      <el-menu-item index="1">首页</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">产品中心</template>
-        <el-menu-item index="2-1">工业路由</el-menu-item>
-        <el-menu-item index="2-2">网关CPE</el-menu-item>
-        <el-menu-item index="2-3">车载产品</el-menu-item>
-        <el-menu-item index="2-4">定位器</el-menu-item>
-        <el-menu-item index="2-5">远程管理平台</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="3">解决方案</el-menu-item>
-      <el-menu-item index="4">服务支持</el-menu-item>
-      <el-menu-item index="5">公司介绍</el-menu-item>
-    </el-menu>
+    <el-container>
+      <el-header height="65px">
+        <el-menu
+          :default-active="activeIndex"
+          class="el-menu"
+          mode="horizontal"
+          background-color="#ffffff"
+          text-color="#000000"
+          active-text-color="#000000"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">产品中心</template>
+            <el-menu-item index="2-1">工业路由</el-menu-item>
+            <el-menu-item index="2-2">网关CPE</el-menu-item>
+            <el-menu-item index="2-3">车载产品</el-menu-item>
+            <el-menu-item index="2-4">定位器</el-menu-item>
+            <el-menu-item index="2-5">远程管理平台</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3">解决方案</el-menu-item>
+          <el-menu-item index="4">服务支持</el-menu-item>
+          <el-menu-item index="5">公司介绍</el-menu-item>
+        </el-menu>
+      </el-header>
+    </el-container>
   </div>
 </template>
 
@@ -41,7 +45,8 @@ export default {
 </script>
 
 <style scoped>
-/* .el-menu > .el-menu-item {
-  float: right;
-} */
+.el-menu {
+  width: 500px;
+  margin-left: 1200px;
+}
 </style>
