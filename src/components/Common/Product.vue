3c0f46330f770890cd04f2ea9af4afef424d6ad7
hu243285237 <243285237@qq.com>
2019-06-18 19:26:23 +0800

    在首页添加产品列表

---
+++ src/components/Common/Product.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="product">
+    <el-container>
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">大屏汽车影音</el-menu-item>
+        <el-menu-item index="2">单锭/双锭汽车影音</el-menu-item>
+        <el-menu-item index="3">小屏汽车影音</el-menu-item>
+        <el-menu-item index="4">汽车数字仪表</el-menu-item>
+        <el-menu-item index="5">汽车定位系统</el-menu-item>
+        <el-menu-item index="6">汽车辅助设备</el-menu-item>
+      </el-menu>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-menu {
+  width: 300px;
+}
+</style>
--- src/components/Page/Index.vue
+++ src/components/Page/Index.vue
@@ -9,33 +9,24 @@
           </el-carousel-item>
         </el-carousel>
         <!-- 产品介绍 -->
-        <div class="context-product">
-          <div class="product">
-            <img class="product-img-left" width="500" height="350" src="../../assets/Images/Carousel/background01.jpg" />
-            <div class="product-text-right">
-              <div class="product-text">
-                <h2>工业路由</h2>
-                  <p>　　工业路由器是一种，利用公用无线网络为用户提供无线的数据传输功能。
-                      已广泛应用于物联网产业链中的M2M行业，如智能电网、智能交通、智能家居、
-                      金融物联网无线通信路由器、移动POS终端、供应链自动化、工业自动化、智能建筑、
-                      消防、公共安全、环境保护、气象、数字化医疗、遥感勘测、农业、林业、水务、煤矿、石化等领域。
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Product/>
       </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
+import Product from '@/components/Common/Product'
+
 import background01 from '../../assets/Images/Carousel/background01.jpg'
 import background02 from '../../assets/Images/Carousel/background02.jpg'
 import background03 from '../../assets/Images/Carousel/background03.jpg'
 
 export default {
   name: 'HelloWorld',
+  components: {
+    Product
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
@@ -51,7 +42,8 @@ export default {
 
 <style scoped>
 .el-main {
-  margin: 0px;
+  margin-left: 250px;
+  margin-right: 250px;
   padding: 0px;
 }
 </style>
